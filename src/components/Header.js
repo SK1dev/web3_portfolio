f20360b6a0b07da1127bfb70cd5862bdb72e459c
SK374 <sk374@protonmail.com>
2022-09-28 17:37:51 +0200

    Add new links for covering letter & CV

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,42 @@
-import profile from '../assets/profile.jpeg';
+import profile from "../assets/profile.jpeg";
 
 const Header = () => {
-    return (
+  return (
+    <section className="header">
+      <img src={profile} alt="Sakari De-Meis" />
 
-        <section className='header'>
-
-            <img src={profile} alt="Sakari De-Meis" />
-
-            <div className='header__content'>
-
-                <h1>Hi, I'm Sakari</h1>
-                <p>
-                Smart Contract & Blockchain Developer 
-                </p>
-                    <br></br>
-                            <a href='mailto:sk374@protonmail.com' className='button'>Email</a>
-                    <br></br>
-                    <br></br>
-                            <a className="storj-link" target="_blank" href="https://link.storjshare.io/jx4x5qsb3t26bsmzsfgimz3wrvza/portfolio-bucket%2FSakari_CV.pdf" attributes-list>CV</a>
-                    <br></br>   
-                        <a className="storj-link" target="_blank" href="https://link.storjshare.io/jxhwbyojqa2keiygiswdueu2pesa/portfolio-bucket%2FSakari_Covering_Letter.pdf" attributes-list>Covering Letter</a>
-                    <br></br>
-                    <br></br>
-                            <small>📍 London, UK/Globally Remote</small>
-                            
-            </div>
-
-        </section>
-        
-    );
-}
+      <div className="header__content">
+        <h1>Hi, I'm Sakari</h1>
+        <p>Smart Contract & Blockchain Developer</p>
+        <br></br>
+        <a href="mailto:sk374@protonmail.com" className="button">
+          Email
+        </a>
+        <br></br>
+        <br></br>
+        <a
+          className="storj-link"
+          target="_blank"
+          href="https://link.storjshare.io/jvfo2tbb67mfblpsomm5ktr2dkxa/portfolio-bucket%2FSakari_CV.pdf"
+          attributes-list
+        >
+          CV
+        </a>
+        <br></br>
+        <a
+          className="storj-link"
+          target="_blank"
+          href="https://link.storjshare.io/ju6luerpatxq4h5jsd4dcc7em52q/portfolio-bucket%2FSakari_Covering_Letter.pdf"
+          attributes-list
+        >
+          Covering Letter
+        </a>
+        <br></br>
+        <br></br>
+        <small>📍 London, UK/Globally Remote</small>
+      </div>
+    </section>
+  );
+};
 
 export default Header;
